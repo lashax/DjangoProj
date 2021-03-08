372ccd3fc7bd370faf8eba4b5e8ed7426e596730
Lasha Sherozia <repeop0@gmail.com>
2021-03-08 18:31:23 +0400

    Added custom pagination class to 'BrandList' view.

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import permissions
 
 from listings.models import Product, Brand
 from listings.serializers import (UserSerializer, ProductSerializer,
                                   RegisterSerializer, BrandSerializer)
+from .paginations import PaginationPerTen
 from .permissions import IsOwnerOrReadOnly
 
 
@@ -47,3 +46,4 @@ class BrandList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+    pagination_class = PaginationPerTen
